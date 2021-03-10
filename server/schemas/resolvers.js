58c0f7fc6c6c2515aea886f0701d570671548c4d
Seth Uschuk <suschuk24@gmail.com>
2021-03-09 22:30:08 -0600

    added resolvers for thoughts, reactions, and friends

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -51,22 +51,63 @@ const resolvers = {
         },
         login: async (parent,{ email, password }) => {
             const user = await User.findOne({ email })
-
+            // if incorrect username
             if(!User) {
                 throw new AuthenticationError("Incorrect credentials")
             }
 
             const correctPw = await user.isCorrectPassword(password);
-
+            // if incorrect password
             if(!correctPw) {
                 throw new AuthenticationError('Incorrect credentials');
             }
-
+            // jwt token
             const token = signToken(user)
 
             return { token, user }
         }, 
-        
+        addThought: async (parent, args, context) => {
+            // if user is logged in
+            if(context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username })
+                // get user id and update thought array
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { thoughts: thought._id } },
+                    // returns updated document
+                    { new: true }
+                );
+                return thought
+            }
+            // if not logged in, throw error
+            throw new AuthenticationError("YUou need to be logged in!")
+        },
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+            if (context.user) {
+              const updatedThought = await Thought.findOneAndUpdate(
+                { _id: thoughtId },
+                { $push: { reactions: { reactionBody, username: context.user.username } } },
+                { new: true, runValidators: true }
+              );
+          
+              return updatedThought;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+          },
+          addFriend: async (parent, { friendId }, context) => {
+              if(context.user) {
+                  const updatedUser = await User.findOneAndUpdate(
+                      { _id: context.user._id },
+                      { $addToSet: { friends: friendId } },
+                      { new: true }
+                  ).populate('friends')
+
+                  return updatedUser
+              }
+
+              throw new AuthenticationError("You need to be logged in to make friends!")
+          }
     }
 };
 
